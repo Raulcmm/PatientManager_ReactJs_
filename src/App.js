7ff64f88fd185939fa40d890313a36dd7221a259
Raúl <osvaldoracm@gmail.com>
2020-07-21 22:16:44 -0500

    final structure and data collection of the form

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Form from './components/Form'
 import AppointmentList from './components/AppointmentList'
 
 const App = () => {
+ 
+  const [appointmentsList, setAppointmentsList] = useState([])
   return (
     <div className="container text-center">
       <h1>Patient Manager</h1>
       <div className="row">
         <div className="col-md-6">
-          <Form/>
+
+          <Form setAppointmentsList={setAppointmentsList}/>
         </div>
         <div className="col-md-6">
-          <AppointmentList/>
+        <h2>{appointmentsList.length === 0 ? "There aren't appointments " : 'Manage your appointments'}</h2>
+          <AppointmentList
+          appointmentsList={appointmentsList}/>
         </div>
       </div>
     </div>
