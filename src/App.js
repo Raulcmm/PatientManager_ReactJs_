f54ca84b27d93a5e532e015f2f86c4a6516aa03f
Raúl <osvaldoracm@gmail.com>
2020-07-22 11:23:29 -0500

    delete added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React, { useState } from 'react'
-import Form from './components/Form'
-import AppointmentList from './components/AppointmentList'
+import React, { useState } from 'react';
+import Form from './components/Form';
+import AppointmentList from './components/AppointmentList';
 
 const App = () => {
- 
-  const [appointmentsList, setAppointmentsList] = useState([])
-  return (
-    <div className="container text-center">
-      <h1>Patient Manager</h1>
-      <div className="row">
-        <div className="col-md-6">
+	
+	const [ appointmentsList, setAppointmentsList ] = useState(initialState);
 
-          <Form setAppointmentsList={setAppointmentsList}/>
-        </div>
-        <div className="col-md-6">
-        <h2>{appointmentsList.length === 0 ? "There aren't appointments " : 'Manage your appointments'}</h2>
-          <AppointmentList
-          appointmentsList={appointmentsList}/>
-        </div>
-      </div>
-    </div>
-  )
-}
+	const deleteAppointment = (id) => {
+		const newApp = appointmentsList.filter((appointment) => appointment.id !== id);
+		setAppointmentsList(newApp);
+	};
 
-export default App
+	return (
+		<div className="container text-center">
+			<h1>Patient Manager</h1>
+			<div className="row">
+				<div className="col-md-6">
+					<Form setAppointmentsList={setAppointmentsList} />
+				</div>
+				<div className="col-md-6">
+					<h2>{appointmentsList.length === 0 ? "There aren't appointments " : 'Manage your appointments'}</h2>
+					<AppointmentList appointmentsList={appointmentsList} deleteAppointment={deleteAppointment} />
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default App;
