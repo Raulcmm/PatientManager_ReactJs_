30a847a219893bf00b24a5c009168f02bd6b8085
Raúl <osvaldoracm@gmail.com>
2020-07-22 11:14:41 -0500

    the index was changed by id of the key attribute

--- src/components/AppointmentList.js
+++ src/components/AppointmentList.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import AppointmentCard from './AppointmentCard';
 
-const AppointmentList = ({ appointmentsList }) => {
+const AppointmentList = ({ appointmentsList,deleteAppointment }) => {
 	if (appointmentsList.length === 0) return null;
 	return <div className="mt-2">
-        {appointmentsList.map((appointment, index) => 
-            <AppointmentCard key={index} appointment={appointment} />)}
+        {appointmentsList.map((appointment) => 
+            <AppointmentCard key={appointment.id} appointment={appointment} deleteAppointment={deleteAppointment} />)}
         </div>;
 };
 
