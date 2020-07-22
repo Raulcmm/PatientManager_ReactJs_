b49ea3fd544b2930fd14245734185945de3a8e8b
Raúl <osvaldoracm@gmail.com>
2020-07-21 21:33:05 -0500

    logic add

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,34 +1,74 @@
-import React from 'react'
+import React from 'react';
+import useForm from '../hooks/useForm';
 
-const Form = () => {
-    return (
-        <div className="mt-2">
-            <h2>Add a new appointment</h2>
-            <form action="" className="text-left">
-                <div className="form-group">
-                    <label>Pet's name</label>
-                    <input type="text" className="form-control" placeholder="Pet's name" name="pet"/>
-                </div>
-                <div className="form-group">
-                    <label>Owner's name</label>
-                    <input type="text" className="form-control" placeholder="Owner's name" name="owner"/>
-                </div>
-                <div className="form-group">
-                    <label>Date</label>{/**Cambiar */}
-                    <input type="date" className="form-control" name="date" />
-                </div>
-                <div className="form-group">
-                    <label>Hour</label>{/**Cambiar */}
-                    <input type="time" className="form-control" name="hour"/>
-                </div>
-                <div className="form-group">
-                    <label>Symptoms</label>{/**Cambiar */}
-                    <textarea name="symptoms"  cols="30" rows="5" className="form-control"></textarea>
-                </div>
-                <button className="btn btn-success float-right" type="submit">Send</button>
-            </form>
-        </div>
-    )
-}
+const Form = ({ setAppointmentsList }) => {
+	const initialState = {
+		pet: '',
+		owner: '',
+		date: '',
+		hour: '',
+		symptoms: ''
+	};
 
-export default Form
+	const { data, handleChange } = useForm(initialState);
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+        setAppointmentsList(appointment=>[...appointment,data])
+        e.currentTarget.reset(); 
+	};
+
+	return (
+		<div className="mt-2">
+			<h2>Add a new appointment</h2>
+			<form onSubmit={handleSubmit} className="text-left">
+				<div className="form-group">
+					<label>Pet's name</label>
+					<input
+						type="text"
+						className="form-control"
+						placeholder="Pet's name"
+						name="pet"
+						onChange={handleChange}
+						required
+					/>
+				</div>
+				<div className="form-group">
+					<label>Owner's name</label>
+					<input
+						type="text"
+						className="form-control"
+						placeholder="Owner's name"
+						name="owner"
+						onChange={handleChange}
+						required
+					/>
+				</div>
+				<div className="form-group">
+					<label>Date</label>
+					<input type="date" className="form-control" name="date" onChange={handleChange} required />
+				</div>
+				<div className="form-group">
+					<label>Hour</label>
+					<input type="time" className="form-control" name="hour" onChange={handleChange} required />
+				</div>
+				<div className="form-group">
+					<label>Symptoms</label>
+					<textarea
+						name="symptoms"
+						cols="30"
+						rows="5"
+						className="form-control"
+						onChange={handleChange}
+						required
+					/>
+				</div>
+				<button className="btn btn-success float-right" type="submit">
+					Send
+				</button>
+			</form>
+		</div>
+	);
+};
+
+export default Form;
