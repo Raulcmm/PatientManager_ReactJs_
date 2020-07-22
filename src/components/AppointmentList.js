d856d9ee4d2ad2e94046c9edb6f3eb77eaf1645a
Raúl <osvaldoracm@gmail.com>
2020-07-21 22:20:39 -0500

    appointentcard implemented

--- src/components/AppointmentList.js
+++ src/components/AppointmentList.js
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from 'react';
+import AppointmentCard from './AppointmentCard';
 
-const AppointmentList = () => {
-    return (
-        <div className="mt-2">
-            <h2>There aren't appointments</h2>
-        </div>
-    )
-}
+const AppointmentList = ({ appointmentsList }) => {
+	if (appointmentsList.length === 0) return null;
+	return <div className="mt-2">
+        {appointmentsList.map((appointment, index) => 
+            <AppointmentCard key={index} appointment={appointment} />)}
+        </div>;
+};
 
-export default AppointmentList
+export default AppointmentList;
